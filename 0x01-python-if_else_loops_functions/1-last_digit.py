61032a7fc3ee340e6ecffb6dbfb65d1c5c663771
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-10 02:30:04 +0300

    shorten variable name to comply with linter rules

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 
 if number >= 0:
-    lastdigit = number % 10
+    lastdgt = number % 10
 else:
-    lastdigit = -1 * ((-1 * number) % 10)
+    lastdgt = -1 * ((-1 * number) % 10)
 
-if lastdigit > 5:
-    print(f"Last digit of {number} is {lastdigit} and is greater than 5")
-elif lastdigit == 0:
-    print(f"Last digit of {number} is {lastdigit} and is 0")
-elif lastdigit < 6 and lastdigit != 0:
-    print(f"Last digit of {number} is {lastdigit} and is less than 6 and not 0")
+if lastdgt > 5:
+    print(f"Last digit of {number} is {lastdgt} and is greater than 5")
+elif lastdgt == 0:
+    print(f"Last digit of {number} is {lastdgt} and is 0")
+elif lastdgt < 6 and lastdgt != 0:
+    print(f"Last digit of {number} is {lastdgt} and is less than 6 and not 0")
