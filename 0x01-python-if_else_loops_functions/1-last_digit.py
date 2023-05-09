39bd8b98875f5fae56ebe4fa7c66a740a15e42b7
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-10 02:26:26 +0300

    why the hell did i add i on 1-last... on 0x01...P

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if lastdigit > 5:
     print(f"Last digit of {number} is {lastdigit} and is greater than 5")
 elif lastdigit == 0:
     print(f"Last digit of {number} is {lastdigit} and is 0")
-elif lastdigit < 6 and i != 0:
+elif lastdigit < 6 and lastdigit != 0:
     print(f"Last digit of {number} is {lastdigit} and is less than 6 and not 0")
