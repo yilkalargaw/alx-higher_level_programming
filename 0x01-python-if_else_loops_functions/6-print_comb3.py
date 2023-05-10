d6c76a8c7967ead55892a57b8cbf75e2fe783dd2
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-10 03:27:50 +0300

    still trying to solve more linter issues on 6-... on 0x01...

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,4 +3,4 @@ for i in range(9):
     for j in range(10):
         if j > i:
             print("{:d}{:d}".format(i, j),
-                  end = "\n" if (i == 8 and j == 9) else ", ")
+                  end="\n" if (i == 8 and j == 9) else ", ")
