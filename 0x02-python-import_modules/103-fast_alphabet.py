2da7e51a505ebfe027addac4764e2fc152387366
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-12 01:08:52 +0000

    iterating on 103... on 0x02...

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    print(list(map(lambda x: chr(x), range(0 + ord('A'), 26 + ord('A')))))
+    from functools import reduce
+    print(reduce(lambda x, y: x + y, map(lambda x: chr(x), range(65, 91))))
