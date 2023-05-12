bdeb6debbcd43ba11d982bdfc8364916c09b6a35
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-12 01:17:29 +0000

    change separator in 103-fast_alphabet on 0x02-python-import_modules

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import string
-    print(*map(lambda x: chr(ord('A') + x), range(26)), sep='\n')
+    print(*map(lambda x: chr(ord('A') + x), range(26)), sep='')
