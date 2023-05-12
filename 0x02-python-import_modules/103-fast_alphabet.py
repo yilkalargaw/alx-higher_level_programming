5bc0f44cac3b587f4e1133c9f06ea06e22d7f3c4
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-12 01:11:47 +0000

    iterating on 103-fast_alphabet on 0x02-python-import_modules

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from functools import reduce
-    print(reduce(lambda x, y: x + y, map(lambda x: chr(x), range(65, 91))))
+    print(*map(lambda x: chr(x), range(65, 91)), sep='')
