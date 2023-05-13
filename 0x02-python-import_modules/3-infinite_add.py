586df7a5c5f5b8ced3ea39cda22b7a085570553f
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-13 15:45:51 +0300

    iterating on 3-infinite_add on 0x02-python-import_modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,4 @@
 if __name__ == "__main__":
     import sys
 
-    j = lambda args: sum(int(i) for i in args)
-
-    print(j(sys.argv[1:]))
+    print((lambda args: sum(int(i) for i in args))(sys.argv[1:]))
