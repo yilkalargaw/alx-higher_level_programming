556def938c821067112a30d1d1ac7c9c0b70167e
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-15 16:39:21 +0000

    attempting 7-add_tuple.py on 0x03-python-data_structures/

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a += (0,) * (2 - len(tuple_a))
+    if len(tuple_b) < 2:
+        tuple_b += (0,) * (2 - len(tuple_b))
+    return (sum((tuple_a[0], tuple_b[0])), sum((tuple_a[1], tuple_b[1])))
