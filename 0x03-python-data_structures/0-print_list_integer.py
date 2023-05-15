299c0122ccc733103b97b2cb64349734451d17bd
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-15 03:42:52 +0000

    add a case for empty list for 0-print_list_integer on 0x03...

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    print(*list(map(lambda x: "{:d}".format(x), my_list)), sep='\n')
+    if (len(my_list) != 0):
+        print(*list(map(lambda x: "{:d}".format(x), my_list)), sep='\n')
