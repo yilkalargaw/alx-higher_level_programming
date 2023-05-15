fdb1f17e45b8b1668efe278cf5c9a7b0c13079fe
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-15 04:30:25 +0000

    add an if guard for 3-print_reversed_list_integer on 0x03...

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    print(*list(map(lambda x: "{:d}".format(x), my_list[::-1])), sep='\n')
+    if (len(my_list) != 0):
+        print(*list(map(lambda x: "{:d}".format(x), my_list[::-1])), sep='\n')
