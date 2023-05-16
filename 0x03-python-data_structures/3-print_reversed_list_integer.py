2ea57588d034220e1d337f90b5d9b8debcfbce2c
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-16 04:47:14 +0000

    add a case for non on 3-print_reversed_list_integer.py on 0x03...

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if (len(my_list) != 0):
+    if my_list==None or len(my_list) == 0:
+        print("")
+    else:
         print(*list(map(lambda x: "{:d}".format(x), my_list[::-1])), sep='\n')
