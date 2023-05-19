cd37362952235c26cdde4579094139a79ec6008b
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-19 00:21:02 +0000

    remove import on 12-roman_to_int on 0x04-python-more_data_structures

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-from functools import reduce
-
-
 def roman_to_int(roman_string):
     nums = dict(zip('MDCLXVI', (1000, 500, 100, 50, 10, 5, 1)))
 
     if isinstance(roman_string, str) and all(i in nums for i in roman_string):
-        return reduce(lambda x, y: x + y if x >= y else x - y,
-                      [nums.get(i, 0) for i in roman_string])
+        equivalents = [nums.get(i, 0) for i in roman_string]
+        return sum(map(lambda x: -x[0] if x[0] < x[1] else x[0],
+                   zip(equivalents, equivalents[1:] + [0])))
+
     return 0
