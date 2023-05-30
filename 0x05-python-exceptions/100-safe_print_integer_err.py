4f10da55c57d5aaa7ae45ef0b227de70a3f81c98
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-30 01:31:39 +0000

    attempting 100-safe_print_integer_err.py on 0x05....

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value), end="\n")
+        return True
+    except (ValueError, TypeError) as x:
+        print("Exception: {}".format(x), file=sys.stderr, end="\n")
+        return False
