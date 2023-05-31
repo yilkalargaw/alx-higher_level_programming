da4d72fc5f0bab8c33ddfa6eb87d9ffc546b48fa
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-31 23:10:58 +0000

    iterating on 2-square.py on 0x06-python-classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -16,8 +16,8 @@ class Square:
         """
 
         if (type(size) is not int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         elif (type(size) is int) and size < 0:
-            raise ValueError("Size must be greater than or equal to 0")
+            raise ValueError("size must be >= 0")
         else:
             self.__size = size
