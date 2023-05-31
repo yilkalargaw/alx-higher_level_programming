7da4afaa039fec933edd5c44e16778c418682b69
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-05-31 02:06:43 +0000

    attempting 1-square.py on 0x06-python-classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square people accepted here"""
+
+
+class Square:
+    """A class to define squareness
+
+    Attributes:
+        __size (int): size of square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+        Arg:
+            size (int): size of square
+        """
+        self.__size = size
