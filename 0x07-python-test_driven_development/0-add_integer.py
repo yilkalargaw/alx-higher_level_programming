b3e1d7a22a76633a922fe0078c173dc1239063e7
yilkalargaw <yilkalargawworkneh@gmail.com>
2023-07-05 00:03:05 +0000

    attempting 0-add_integer.txt on 0x07-python-test_driven_development

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Addition Function
+"""
+
+
+def add_integer(a, b=98):
+    """this adds 2 int numbers
+    Arg:
+        a:
+        b:
+    Return:
+        a + b
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+
+    return (a + b)
