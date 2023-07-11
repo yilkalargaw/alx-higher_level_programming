4f66a165ad708e19fd3ab8c0c0c18c12ced65c83
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-07-11 04:17:10 +0300

    attempting 0x0A-python-inheritance/6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Inheritance Practice"""
+
+
+class BaseGeometry():
+    """Currently Empty"""
+
+    def area(self):
+        """ currently raises an error
+        Arg:
+
+        Return:
+          True or False
+        """
+        raise Exception("area() is not implemented")
