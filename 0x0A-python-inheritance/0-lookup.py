559e6247f9912542444983da4e094ba90bd559da
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-07-11 03:11:56 +0300

    attempting 0x0A-python-inheritance/0-lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Inheritance Practice"""
+
+
+def lookup(obj):
+    """Returns a list of attributes and methods in an object
+    Arg:
+        obj:
+
+    Return:
+        [objects]
+    """
+    return (dir(obj))
