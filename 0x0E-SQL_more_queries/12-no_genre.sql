4d477ca9d83500c54d0c0b5314aa2adae635922e
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-08-16 22:16:47 +0000

    attempting 0x0E-SQL_more_queries/12-no_genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- list shows contained in hbtn_0d_tvshows
+SELECT a.title, b.genre_id
+FROM tv_shows a
+LEFT JOIN tv_show_genres b
+ON a.id = b.show_id
+WHERE b.genre_id IS NULL
+ORDER BY a.title ASC, b.genre_id ASC;
