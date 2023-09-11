cf4558430f178f89c9d8753bcbe52fafa1f82271
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-11 21:54:02 +0000

    just use iterative solution for 0x12-javascript-warm_up/10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 function factorial(x) {
-  var memo = {};
-  function factorial(n) {
-    return n < 2 ? 1 : memo[n] || (memo[n] = n * factorial(n - 1));
-  }
-  return factorial(x);
+	if (x < 0) {
+		return 1;
+	}
+	var result = 1;
+	while (x > 1) {
+		result *= x;
+		x--;
+	}
+	return result;
 }
 
-console.log(factorial(Number(process.argv[2])))
+console.log(factorial(Number(process.argv[2])));
