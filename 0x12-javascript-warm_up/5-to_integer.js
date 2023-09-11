b65742cce890c9c011a2cf2e57028ec77882c12b
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-11 22:34:48 +0000

    semistandard modifications on 0x12-Javascript-Warm_Up/5-To_Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const val = parseInt(process.argv[2]);
 if (isNaN(Number(val))) {
-	console.log('Not a number');
+  console.log('Not a number');
 } else {
-	console.log('My number:' + ' ' + val);
+  console.log('My number:' + ' ' + val);
 }
