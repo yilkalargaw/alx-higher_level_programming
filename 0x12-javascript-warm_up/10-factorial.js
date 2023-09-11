4f2fa36460bb0c77ff3df809456800768448e2aa
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-11 21:56:54 +0000

    untabify 0x12-javascript-warm_up/10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 function factorial(x) {
-	if (x < 0) {
-		return 1;
-	}
-	var result = 1;
-	while (x > 1) {
-		result *= x;
-		x--;
-	}
-	return result;
+  if (x < 0) {
+    return 1;
+  }
+  var result = 1;
+  while (x > 1) {
+    result *= x;
+    x--;
+  }
+  return result;
 }
 
 console.log(factorial(Number(process.argv[2])));
