be49a64cae1a911057da8d94ab3ed14bb3122ec8
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-11 21:33:16 +0000

    attemping 0x12-javascript-warm_up/10-factorial.js

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,9 @@
+function factorial(x) {
+  var memo = {};
+  function factorial(n) {
+    return n < 2 ? 1 : memo[n] || (memo[n] = n * factorial(n - 1));
+  }
+  return factorial(x);
+}
+
+console.log(factorial(Number(process.argv[2])))
