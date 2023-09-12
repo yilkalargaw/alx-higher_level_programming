753c97676328a518a17e2e5d28db0a99db86475b
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-12 17:59:39 +0000

    still iterating on 0x13-javascript_objects_scopes_closures/2-rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || w === undefined || h === undefined {
+    if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
       w = {};
       h = {};
     } else {
