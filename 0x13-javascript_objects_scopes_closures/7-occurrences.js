47704f3b63a9b680a58bab35c1d1e0eddac21185
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-12 17:20:08 +0000

    attempting 0x13-javascript_objects_scopes_closures/7-occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,7 +2,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let x = 0;
   for (let i = 0; i < list.length; i++) {
-    list[i] === searchElement ? x++ : x, 0;
+    if (list[i] === searchElement) { x++; }
   }
   return (x);
 };
