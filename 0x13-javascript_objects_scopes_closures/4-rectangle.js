1706428066adaf6c6b4ea7172b786892c1b69205
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-12 16:55:51 +0000

    attempting 0x13-javascript_objects_scopes_closures/4-rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -15,8 +15,7 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    this.width = this.height;
-    this.height = this.width;
+    [this.width, this.height] = [this.height, this.width];
   }
 
   double () {
