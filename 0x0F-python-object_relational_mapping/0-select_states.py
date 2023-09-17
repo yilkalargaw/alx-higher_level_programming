f239987b80d9f78b833d7e20c9c4502c74cc1814
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-17 23:47:02 +0000

    move evertyihing to inbetween if __name__ == '__main__': for 0x0F-python-object_relational_mapping/0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 """List all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
-import sys
-
-def select_states():
-    """Fetches and returns all states from the database"""
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-
-    with db.cursor() as cursor:
-        cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
-        return cursor.fetchall()
-
 if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    def select_states():
+        """Fetches and returns all states from the database"""
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+
+        with db.cursor() as cursor:
+            cursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
+            return cursor.fetchall()
+
     states = select_states()
     for state in states:
         print(state)
