5910357e15b0ce69dac285a8f60052be8ee28550
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-18 00:22:50 +0000

    attempting 0x0F-python-object_relational_mapping/5-filter_cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""List all states from the database hbtn_0e_0_usa"""
+
+if __name__ == '__main__':
+
+    import MySQLdb
+    import sys
+
+    def select_states():
+        """Fetches and returns all states from the database"""
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+
+        with db.cursor() as cursor:
+            cursor.execute("SELECT cities.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                WHERE states.name = %s\
+                ORDER BY cities.id ASC", (sys.argv[4],))
+            return cursor.fetchall()
+
+    states = select_states()
+    for state in states:
+        print(state)
