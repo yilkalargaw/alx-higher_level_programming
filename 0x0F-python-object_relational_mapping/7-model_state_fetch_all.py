c60ab38e53c311f1dc18f1981d9c58fd5475c876
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-09-18 02:45:24 +0000

    still iterating on 0x0F-python-object_relational_mapping/7-model_state_fetch_all

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -11,10 +11,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
 
-    states = session.query(State).all()
-
-    for i in states:
-        print("{}: {}".format(i.id, i.name))
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
