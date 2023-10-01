3fee0b5315e4ceb46a1785e5ab98f1694ee4b86b
Yilkal Argaw <yilkalargawworkneh@gmail.com>
2023-10-02 01:09:32 +0300

    iterating on 0x10-python-network_0/0-body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -I $1 | grep "Content-Length" | cut -d " " -f2
